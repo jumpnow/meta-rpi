b30bdff50ef48f0b82727c4fdc5259fa1593c6bd
Scott Ellis <scott@jumpnowtek.com>
2016-01-31 17:04:09 -0500

    create sdcard script: Handle some different card sizes

--- scripts/create_sdcard_images.sh
+++ scripts/create_sdcard_images.sh
@@ -5,13 +5,6 @@ IMG=qt5-x11
 IMG_LONG="${IMG}-image-${MACHINE}"
 HOSTNAME=rpi2
 
-get_version()
-{
-	local fullpath=`ls ${SRCDIR}/${IMG_LONG}-*.rootfs.tar.bz2`
-	local version=`basename $fullpath .rootfs.tar.bz2 | awk -F'-' '{ print $5 }'`	
-	VERSION=$version	
-}
-
 if [ "x${1}" = "x" ]; then
 	echo "Usage: ${0} <block device>"
 	exit 1
@@ -28,7 +21,22 @@ if [ ! -b /dev/${DEV} ]; then
 	echo "Block device not found: /dev/${DEV}"
 	exit 1
 fi
-	
+
+if [ "x${2}" = "x" ]; then
+	CARDSIZE=2
+else
+	if [ "${2}" -eq 2 ]; then
+		CARDSIZE=2
+	elif [ "${4}" -eq 4 ]; then
+		CARDSIZE=4
+	elif [ "${2}" -gt 4 ]; then
+		CARDSIZE=8
+	else
+		echo "Unsupported card size: ${2}"
+		exit 1
+	fi
+fi
+
 if [ -z "${OETMP}" ]; then
 	echo "OETMP environment variable not set"
 	exit 1
@@ -55,29 +63,42 @@ if [ ! -f "${SRCDIR}/${IMG_LONG}.tar.bz2" ]; then
 	exit 1
 fi
 
-get_version
+if [ "${CARDSIZE}" -eq 8 ]; then
+	echo -e "\n***** Zeroing 8GB of the SD card *****\n"
+	sudo dd if=/dev/zero of=/dev/${DEV} bs=1M count=7400
+elif [ "${CARDSIZE}" -eq 4 ]; then
+	echo -e "\n***** Zeroing 4GB of the SD card *****\n"
+	sudo dd if=/dev/zero of=/dev/${DEV} bs=1M count=3600
+elif [ "${CARDSIZE}" -eq 2 ]; then
+	echo -e "\n***** Zeroing 2GB of the SD card *****\n"
+	sudo dd if=/dev/zero of=/dev/${DEV} bs=1M count=1800
+else
+	echo "Unsupported card size: ${CARDSIZE}"
+	exit 1
+fi
 
-echo -e "\n***** Zeroing the 2GB SD card *****\n"
-sudo dd if=/dev/zero of=/dev/${DEV} bs=1M count=1800 
-
-echo -e "\n***** Partitioning the SD card *****\n"
+echo -e "\n***** Partitioning the SD card *****"
 sudo ./mk2parts.sh ${DEV} 
 
-echo -e "\n***** Copying the boot partition *****\n"
+echo -e "\n***** Copying the boot partition *****"
 ./copy_boot.sh ${DEV} 
 
-echo -e "\n***** Copying the installer rootfs *****\n"
+echo -e "\n***** Copying the rootfs *****"
 ./copy_rootfs.sh ${DEV} ${IMG} ${HOSTNAME} 
 
 sleep 5
 
 echo -e "\n***** Copying the SD card image *****\n"
-sudo dd if=/dev/${DEV} of=${DSTDIR}/${IMG}-${MACH}-${VERSION}.img bs=1M
+FULL_NAME=${IMG}-${MACH}-${CARDSIZE}gb.img
 
-echo -e "\n***** Compressing the SD card image *****\n"
-sudo xz -9 ${DSTDIR}/${IMG}-${MACH}-${VERSION}.img
+sudo dd if=/dev/${DEV} of=${DSTDIR}/${FULL_NAME} bs=1M
 
-echo -e "\n***** Creating an md5sum *****\n"
-md5sum ${DSTDIR}/${IMG}-${MACH}-${VERSION}.img.xz > ${DSTDIR}/${IMG}-${MACH}-${VERSION}.img.xz.md5
+echo -e "\n***** Compressing the SD card image *****"
+sudo xz -9 ${DSTDIR}/${FULL_NAME}
+
+echo -e "\n***** Creating an md5sum *****"
+cd ${DSTDIR}
+md5sum ${FULL_NAME}.xz > ${FULL_NAME}.xz.md5
+cd ${OLDPWD}
 
 echo -e "\n***** Done *****\n"
