f5e262a856876d7f0c394ba59469513f33ef7794
Scott Ellis <scott@jumpnowtek.com>
2016-08-30 11:22:02 -0400

    qt5-env: Add some QPA variables, default to eglfs for platform

--- recipes-qt/qt5-env/files/qt5-env.sh
+++ recipes-qt/qt5-env/files/qt5-env.sh
@@ -2,3 +2,8 @@
 
 export PATH=${PATH}:/usr/bin/qt5
 
+export QT_QPA_PLATFORM=eglfs
+
+# for the official Pi 7" touchscreen, the units are mm
+#export QT_QPA_EGLFS_PHYSICAL_WIDTH=155
+#export QT_QPA_EGLFS_PHYSICAL_HEIGHT=86
--- recipes-qt/qt5-env/qt5-env.bb
+++ recipes-qt/qt5-env/qt5-env.bb
@@ -5,6 +5,8 @@ LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=801f80980d171dd6425
 
 SRC_URI = "file://qt5-env.sh"
 
+PR = "r1"
+
 S = "${WORKDIR}"
 
 do_install() {
