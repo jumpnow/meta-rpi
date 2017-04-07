1aee88825034664fedcc6e842cff03744b87b06f
Scott Ellis <scott@jumpnowtek.com>
2017-04-07 18:05:15 -0400

    copy_rootfs: Use local interfaces and wpa_supplicant.conf if found

--- scripts/copy_rootfs.sh
+++ scripts/copy_rootfs.sh
@@ -88,11 +88,17 @@ sudo -E bash -c 'echo ${TARGET_HOSTNAME} > /media/card/etc/hostname'
 if [ -f ${SRCDIR}/interfaces ]; then
 	echo "Writing interfaces to /media/card/etc/network/"
 	sudo cp ${SRCDIR}/interfaces /media/card/etc/network/interfaces
+elif [ -f ./interfaces ]; then
+	echo "Writing ./interfaces to /media/card/etc/network/"
+	sudo cp ./interfaces /media/card/etc/network/interfaces
 fi
 
 if [ -f ${SRCDIR}/wpa_supplicant.conf ]; then
 	echo "Writing wpa_supplicant.conf to /media/card/etc/"
 	sudo cp ${SRCDIR}/wpa_supplicant.conf /media/card/etc/wpa_supplicant.conf
+elif [ -f ./wpa_supplicant.conf ]; then
+	echo "Writing ./wpa_supplicant.conf to /media/card/etc/"
+	sudo cp ./wpa_supplicant.conf /media/card/etc/wpa_supplicant.conf
 fi
 
 echo "Unmounting ${DEV}"
